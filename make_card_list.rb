f44664427cad739e406c982bc069e63e083cf8fb
Warren Bain <warren@ninefold.com>
2016-02-19 21:30:14 +1100

    New approach to make files

--- make_card_list.rb
+++ make_card_list.rb
@@ -11,18 +11,81 @@
 #   english equivalent
 #
 # and we want to produce each pair on a single line as
+#
 #   english term<tab>spanish term
+#
+# and in another file as
+#
+#   spanish term<tab>english term
+#
 
-old_file = ARGV.first
-new_file = File.basename(old_file, '.*') + '.new'
-cards = []
+require 'optparse'
 
-File.open(old_file, 'r') do |f|
-  f.each_line.each_slice(2) do |definition, term|
-    cards << "#{term.chomp}\t#{definition.chomp}\n"
+def write_cards_to_file(file, cards=[], sort=true)
+  File.open(file,'w') do |f|
+    f.puts sort ? cards.sort : cards
   end
 end
 
-File.open(new_file,'w') do |f|
-  f.puts(cards.sort)
+def file_name(part_a, part_b, part_c)
+  part_a + '_' + part_b + '_' + part_c + '.fc'
 end
+
+options = {
+  list: %w(spanish english),
+  sort: false
+}
+
+optparse = OptionParser.new do |opts|
+  opts.banner = "Usage: make_card_list.rb [options]"
+
+  opts.separator ""
+  opts.separator "Specific options:"
+
+  opts.on("-f", "--file FILE", "Input file to be converted", "=MANDATORY")  { |f| options[:file] = f }
+  opts.on("-s", "--[no-]sort", "Sort output files by terms")                { |s| options[:sort] = s }
+  opts.on("-l", "--list a,b", Array, "Language terms order in file")        { |l| options[:list] = l }
+
+  opts.separator ""
+  opts.separator "Common options:"
+
+  opts.on_tail("-h", "--help", "Show this message") do
+    puts opts
+    exit
+  end
+end
+
+begin
+  optparse.parse!
+  mandatory = [:file]
+  missing = mandatory.select { |o| options[o].nil? }
+  unless missing.empty?
+    puts "Missing options: #{missing.join(', ')}"
+    puts optparse
+    exit 1
+  end
+rescue OptionParser::InvalidOption, OptionParser::MissingArgument
+  puts $!.to_s
+  puts optparse
+  exit 1
+end
+
+a_cards = []
+b_cards = []
+
+File.open(options[:file], 'r') do |f|
+  f.each_line.each_slice(2) do |term, definition|
+    a_cards << "#{definition.chomp}\t#{term.chomp}\n"
+    b_cards << "#{term.chomp}\t#{definition.chomp}\n"
+  end
+end
+
+write_cards_to_file file_name(options[:list].first,
+                              options[:list].last,
+                              File.basename(options[:file], '.*')
+                             ), a_cards, options[:sort]
+
+write_cards_to_file file_name(options[:list].last,
+                              options[:list].first,
+                              File.basename(options[:file], '.*')
+                             ), b_cards, options[:sort]
